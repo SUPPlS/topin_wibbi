8f51167c0350524b49d4cbabe0c642b1fa20e479
Topi Ilmasti <122445154+SUPPlS@users.noreply.github.com>
2023-10-23 17:21:12 +0300

    guys on valmis
    guys lisatty

--- assets/javascript/sound.js
+++ assets/javascript/sound.js
@@ -2,13 +2,13 @@ var audio = document.getElementById("audio");
 var playButton = document.getElementById("playButton");
 
 playButton.addEventListener("click", function() {
-
+    console.log("bissi");
     if (audio.paused) {
         audio.play();
-        playButton.textContent = "Pysäytä ääni";
+        playButton.textContent = "guys";
     } else {
         audio.pause();
         audio.currentTime = 0;
-        playButton.textContent = "Toista ääni";
+        playButton.textContent = "guys";
     }
 });
